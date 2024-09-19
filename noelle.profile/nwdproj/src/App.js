d2528ebc876c499c537d5a1b3fbc71cde77bc05f
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-19 15:01:04 +0800

    Card

--- noelle.profile/nwdproj/src/App.js
+++ noelle.profile/nwdproj/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import Carrd from './Carrd';
-
-const profileData = {
-  profilePicture: "profile-card" 
-};
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <Carrd icon="download.jpg"name="Noelle Fontanar" bio="A comp sci student with a love for coffee"/>
-      <Carrd icon="download2.jpg"name="Anne Santos" bio="An arts student with a love for tea"/>
-      <Carrd icon="download3.jpg"name="Nana Fuentabilla" bio="A student with a love for water"/>
+      <Card icon="download.jpg"name="Noelle Fontanar" bio="A comp sci student with a love for coffee"/>
+      <Card icon="download2.jpg"name="Anne Santos" bio="An arts student with a love for tea"/>
+      <Card icon="download3.jpg"name="Nana Fuentabilla" bio="A student with a love for water"/>
     </div>
   );
 }
